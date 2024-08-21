607cef21b68130ec6f4fd761b6a3e469a602a06c
Encoded <75113441+EncodedClass@users.noreply.github.com>
2024-08-21 17:03:23 +0200

    init

---
+++ navbarManager.js
@@ -0,0 +1,8 @@
+const nonHamburgerNavbar = document.querySelector("#regularNavbar");
+const HamburgerNavbar = document.querySelector("#hamburgerNavbar");
+
+function reportWindowSize() {
+  
+}
+
+window.onresize = reportWindowSize;
