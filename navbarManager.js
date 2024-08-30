65bc53c334f026f52a2eecda918d0fd14ee5acce
Encoded <75113441+EncodedClass@users.noreply.github.com>
2024-08-30 13:30:35 +0200

    idk man... content

--- navbarManager.js
+++ navbarManager.js
@@ -1,8 +1,17 @@
 const nonHamburgerNavbar = document.querySelector("#regularNavbar");
 const HamburgerNavbar = document.querySelector("#hamburgerNavbar");
 
+function moveAllContent(oldParent, newParent)
+{
+    while (oldParent.hasChildNodes()) newParent.appendChild(oldParent.firstChild);
+}
+
+var mediatest1 = window.matchMedia("(-webkit-min-device-pixel-ratio: 2), (min--moz-device-pixel-ratio: 2), (min-resolution: 2dppx), (min-resolution: 192dpi), (max-width: 580px)")
 function reportWindowSize() {
-  
+  if(mediatest1.matches)
+  {
+    console.log("test")
+  }
 }
 
 window.onresize = reportWindowSize;
